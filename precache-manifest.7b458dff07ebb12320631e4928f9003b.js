06201fb636bdb4bf959a7df6390cc09137282350
minerva <minerva@pop-os.localdomain>
2023-02-03 16:13:12 -0300

    Updates

---
+++ precache-manifest.7b458dff07ebb12320631e4928f9003b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d882322d589636e4cbd",
+    "url": "/First-React-Router-App/static/css/main.fce2d881.chunk.css"
+  },
+  {
+    "revision": "9d882322d589636e4cbd",
+    "url": "/First-React-Router-App/static/js/main.9d882322.chunk.js"
+  },
+  {
+    "revision": "0f42ad21f20e3536abc0",
+    "url": "/First-React-Router-App/static/js/1.0f42ad21.chunk.js"
+  },
+  {
+    "revision": "497d72b33df418d33994",
+    "url": "/First-React-Router-App/static/css/2.dd7910b7.chunk.css"
+  },
+  {
+    "revision": "497d72b33df418d33994",
+    "url": "/First-React-Router-App/static/js/2.497d72b3.chunk.js"
+  },
+  {
+    "revision": "ca290aa93390db94c980",
+    "url": "/First-React-Router-App/static/js/runtime~main.ca290aa9.js"
+  },
+  {
+    "revision": "0ed6faa4330c1e45c4f8bc5080137b9c",
+    "url": "/First-React-Router-App/index.html"
+  }
+];
